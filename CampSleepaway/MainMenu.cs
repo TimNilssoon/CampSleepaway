27888cc2890a182b5db9288ffd260f47edf2b05e
MaximusTaHan <max.95hansen@gmail.com>
2023-01-09 14:15:05 +0100

    Setup menu

---
+++ CampSleepaway/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿namespace CampSleepaway
+{
+    public class MainMenu
+    {
+        public static void Menu()
+        {
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("---------");
+
+                int selection = HelperMethods.ShowMenu("What would you like to do?", new string[]
+                {
+                    ""
+                });
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
