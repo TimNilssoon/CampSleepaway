8911ab42dcd2f0b7787daff1643c3db0bb3a4c94
MaximusTaHan <max.95hansen@gmail.com>
2023-01-10 13:10:58 +0100

    Text edit

--- CampSleepaway/Menus/CouncelorsMenu.cs
+++ CampSleepaway/Menus/CouncelorsMenu.cs
@@ -32,7 +32,7 @@ namespace CampSleepaway.Menus
                 options.Add(councelor.GetFullName());
             }
 
-            int selection = HelperMethods.ShowMenu("Which camper would you like to manage?", options);
+            int selection = HelperMethods.ShowMenu("Which Councelor would you like to manage?", options);
 
             ManageCouncelorMenu(councelors[selection]);
         }
@@ -58,8 +58,8 @@ namespace CampSleepaway.Menus
                 "Modify Phone Number",
                 "Modify Start Date",
                 "Modify End Date",
-                "Move Camper to another Camp",
-                "Delete Camper"
+                "Move Councelor to another Camp",
+                "Delete Councelor"
             });
 
             switch (selection)
@@ -70,6 +70,20 @@ namespace CampSleepaway.Menus
                 default:
                     break;
             }
+        }
+
+        private static void ModifyPhoneNumber(Councelor camper)
+        {
+            string newPhoneNumber = HelperMethods.GetString("Enter new phone number:");
+            using CampSleepawayContext context = new();
+
+            var camperDb = context.Councelors.SingleOrDefault(c => c.CouncelorId == camper.CouncelorId);
+
+            camperDb!.PhoneNumber = newPhoneNumber;
+
+            context.SaveChanges();
+
+            HelperMethods.ShowMessage("Updated phone number!");
         }
     }
 }
