00f2d4dc6e3923b87cf901caa410a1d3281bd46c
TimNilssoon <tim.nilssoon@outlook.com>
2023-01-11 15:18:39 +0100

    Add Cabinmenu

---
+++ CampSleepaway/Menus/CabinsMenu.cs
@@ -0,0 +1,36 @@
+﻿using CampSleepaway.Controller;
+using CampSleepaway.Menus.CamperMenus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Menus
+{
+    public class CabinsMenu
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+
+            int selection = HelperMethods.ShowMenu("What would you like to do?", new[]
+            {
+                "Add Cabin",
+                "Edit Cabin"
+            });
+
+            switch (selection)
+            {
+                case 0:
+                    CabinController.AddCabin();
+                    break;
+                case 1:
+                    EditCamperMenu.Menu();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
