0d9b4740e942f7a56111a31fce714f70a7e6eda7
Max Hansen <meepmopo@gmail.com>
2023-01-11 13:30:47 +0100

    Display councelors menu

---
+++ CampSleepaway/Menus/CouncelorMenus/ListCouncelorsMenu.cs
@@ -0,0 +1,40 @@
+﻿using CampSleepaway.Controller;
+using CampSleepaway.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Menus.CouncelorMenus
+{
+    public class ListCouncelorsMenu
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+
+            List<Councelor> councelors = CouncelorController.GetCouncelors();
+            List<string> options = new();
+
+            foreach (var councelor in councelors)
+            {
+                options.Add(councelor.GetFullName());
+            }
+
+            int selection = HelperMethods.ShowMenu("Select a camper to display their information", options);
+
+            DisplayCouncelorInfo(councelors[selection]);
+        }
+        private static void DisplayCouncelorInfo(Councelor councelor)
+        {
+            Console.Clear();
+
+            string info = councelor.GetInfo();
+
+            Console.WriteLine(info);
+
+            HelperMethods.ShowMessage();
+        }
+    }
+}
