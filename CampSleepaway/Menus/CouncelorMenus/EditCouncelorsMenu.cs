f1e7cbff38c398288f141622c93b05b9b3de3d63
Max Hansen <meepmopo@gmail.com>
2023-01-11 13:21:53 +0100

    Refactored councelor menu

---
+++ CampSleepaway/Menus/CouncelorMenus/EditCouncelorsMenu.cs
@@ -0,0 +1,86 @@
+﻿using CampSleepaway.Controller;
+using CampSleepaway.Data;
+using CampSleepaway.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Menus
+{
+    public class EditCouncelorsMenu
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+
+            List<Councelor> councelors = CouncelorController.GetCouncelors();
+            List<string> options = new();
+
+            foreach (var councelor in councelors)
+            {
+                options.Add(councelor.GetFullName());
+            }
+
+            int selection = HelperMethods.ShowMenu("Which Councelor would you like to manage?", options);
+
+            ManageCouncelorMenu(councelors[selection]);
+        }
+
+        public static void ManageCouncelorMenu(Councelor councelor)
+        {
+            int councelorId = councelor.CouncelorId;
+
+            Console.Clear();
+            string temp = "None";
+            if (councelor.Cabin is not null)
+            {
+                temp = councelor.Cabin.Name;
+            }
+
+            string title = $"{councelor.FirstName} {councelor.LastName}\n" +
+                $"Phone Number: {councelor.PhoneNumber}\n" +
+                $"Start Date: {councelor.StartDate:yyyy-MM-dd}\n" +
+                $"End Date: {councelor.EndDate:yyyy-MM-dd}\n" +
+                $"Favorite Number: {councelor.FavoriteNumber}" +
+                $"Camp: {temp}\n";
+
+            int selection = HelperMethods.ShowMenu(title, new[]
+            {
+                "Modify Phone Number",
+                "Modify Start Date",
+                "Modify End Date",
+                "Move Councelor to another Camp",
+                "Delete Councelor"
+            });
+
+            switch (selection)
+            {
+                case 0:
+                    string newPhoneNumber = PersonController.ProccessPhoneNumber();
+                    CouncelorController.UpdatePhoneNumber(councelorId, newPhoneNumber);
+                    break;
+                case 1:
+                    DateTime newStartDate = PersonController.ProccessStartDate();
+                    CouncelorController.UpdateCouncelorStartDate(councelorId, newStartDate);
+                    break;
+                case 2:
+                    DateTime newEndDate = PersonController.ProccessEndDate();
+                    CouncelorController.UpdateCouncelorEndDate(councelorId,newEndDate);
+                    break;
+                case 3:
+                    CouncelorController.UpdateFavoriteNumber(councelorId);
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    CouncelorController.DeleteCouncelor(councelorId);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- CampSleepaway/Menus/CouncelorsMenu.cs
+++ CampSleepaway/Menus/CouncelorsMenu.cs
@@ -1,7 +1,4 @@
 ﻿using CampSleepaway.Controller;
-using CampSleepaway.Data;
-using CampSleepaway.Model;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,67 +13,23 @@ namespace CampSleepaway.Menus
         {
             Console.Clear();
 
-            List<Councelor> councelors = CouncelorController.GetCouncelors();
-            List<string> options = new();
-
-            foreach (var councelor in councelors)
+            int selection = HelperMethods.ShowMenu("What would you like to do?", new[]
             {
-                options.Add(councelor.GetFullName());
-            }
-
-            int selection = HelperMethods.ShowMenu("Which Councelor would you like to manage?", options);
-
-            ManageCouncelorMenu(councelors[selection]);
-        }
-
-        public static void ManageCouncelorMenu(Councelor councelor)
-        {
-            int councelorId = councelor.CouncelorId;
-
-            Console.Clear();
-            string temp = "None";
-            if (councelor.Cabin is not null)
-            {
-                temp = councelor.Cabin.Name;
-            }
-
-            string title = $"{councelor.FirstName} {councelor.LastName}\n" +
-                $"Phone Number: {councelor.PhoneNumber}\n" +
-                $"Start Date: {councelor.StartDate:yyyy-MM-dd}\n" +
-                $"End Date: {councelor.EndDate:yyyy-MM-dd}\n" +
-                $"Favorite Number: {councelor.FavoriteNumber}" +
-                $"Camp: {temp}\n";
-
-            int selection = HelperMethods.ShowMenu(title, new[]
-            {
-                "Modify Phone Number",
-                "Modify Start Date",
-                "Modify End Date",
-                "Move Councelor to another Camp",
-                "Delete Councelor"
+                "List all Councelors",
+                "Add Councelor",
+                "Edit Councelor"
             });
 
             switch (selection)
             {
                 case 0:
-                    string newPhoneNumber = PersonController.ProccessPhoneNumber();
-                    CouncelorController.UpdatePhoneNumber(councelorId, newPhoneNumber);
+                    ListCampersMenu.Menu();
                     break;
                 case 1:
-                    DateTime newStartDate = PersonController.ProccessStartDate();
-                    CouncelorController.UpdateCouncelorStartDate(councelorId, newStartDate);
+                    CouncelorController.AddCouncelor();
                     break;
                 case 2:
-                    DateTime newEndDate = PersonController.ProccessEndDate();
-                    CouncelorController.UpdateCouncelorEndDate(councelorId,newEndDate);
-                    break;
-                case 3:
-                    CouncelorController.UpdateFavoriteNumber(councelorId);
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    CouncelorController.DeleteCouncelor(councelorId);
+                    EditCouncelorsMenu.Menu();
                     break;
                 default:
                     break;
