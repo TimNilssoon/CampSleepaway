8eb3463fcffdb8298a1c2357ca059c8e38553b0b
TimNilssoon <tim.nilssoon@outlook.com>
2023-01-11 13:20:39 +0100

    Move AddCamper method to CamperController

--- CampSleepaway/Controller/CamperController.cs
+++ CampSleepaway/Controller/CamperController.cs
@@ -28,6 +28,48 @@ namespace CampSleepaway.Controller
             return context.Campers.Single(c => c.CamperId == id);
         }
 
+        public static void AddCamper()
+        {
+            Console.Clear();
+
+            Camper camper = new();
+
+            camper.FirstName = HelperMethods.GetString("First Name:");
+            camper.LastName = HelperMethods.GetString("Last Name:");
+            camper.PhoneNumber = HelperMethods.GetString("Phone Number:");
+            camper.DateOfBirth = HelperMethods.GetDateTime("Date of Birth (yyyy-MM-dd):");
+            camper.StartDate = HelperMethods.GetDateTime("Start Date:");
+            camper.EndDate = HelperMethods.GetDateTime("End Date:");
+
+            bool addToCabin = HelperMethods.GetBool("Do you want to assign this camper to a cabin? (true/false)");
+
+            if (addToCabin)
+            {
+                Console.WriteLine();
+                List<Cabin> cabins = CabinController.GetCabins();
+                List<string> cabinNames = cabins.Select(c => c.Name).ToList();
+
+                int selection = HelperMethods.ShowMenu("Select new cabin", cabinNames);
+
+                camper.CabinId = cabins[selection].CabinId;
+            }
+
+            string prompt = "Save changes to Db? (true/false)";
+            bool save = HelperMethods.GetBool(prompt);
+            if (save)
+            {
+                using CampSleepawayContext context = new();
+                context.Campers.Add(camper);
+                context.SaveChanges();
+
+                HelperMethods.ShowMessage($"{camper.FirstName} {camper.LastName} was created!");
+            }
+            else
+            {
+                HelperMethods.ShowMessage("Person was not added...");
+            }
+        }
+
         public static void UpdateCamperStartDate(int id, DateTime newStartTime)
         {
             using CampSleepawayContext context = new();
