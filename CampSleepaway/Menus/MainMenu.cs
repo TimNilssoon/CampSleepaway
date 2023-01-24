c5fdf9b304db9dc1d0ab94caa0c0c9e55da6b6dd
Tim Nilsson <tim.nilssoon@outlook.com>
2023-01-24 10:28:21 +0100

    Add "View History" in main menu

--- CampSleepaway/Menus/MainMenu.cs
+++ CampSleepaway/Menus/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using CampSleepaway.Menus.CabinMenus;
 using CampSleepaway.Menus.CamperMenus;
 using CampSleepaway.Menus.CouncelorMenus;
+using CampSleepaway.Menus.HistoryMenus;
 using CampSleepaway.Menus.VistorMenus;
 using System.Net.Security;
 
@@ -28,6 +29,7 @@ namespace CampSleepaway.Menus
                     "Cabin Reports",
                     "Councelor Reports",
                     "Visitor Information",
+                    "View History",
                     "Exit"
                 });
 
@@ -52,6 +54,9 @@ namespace CampSleepaway.Menus
                         VisitorInfoMenu.Menu();
                         break;
                     case 6:
+                        HistoryMenu.Menu();
+                        break;
+                    case 7:
                         running = false;
                         break;
                     default:
